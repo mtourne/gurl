06f49546b02bfd7bb2fef84d24897796c989cb4f
Matthieu Tourne <matthieu@cloudflare.com>
2012-08-11 02:06:34 -0700

    Add a goroutine to gather the output from parallel requests

--- gurl.go
+++ gurl.go
@@ -11,9 +11,9 @@ import (
 	"time"
 )
 
-
 var VERBOSE = 0
 
+// Connect loop
 func connect(start, done chan bool, url string) {
 
 	for {
@@ -42,16 +42,42 @@ func connect(start, done chan bool, url string) {
 
 		// Print the body
 		if VERBOSE > 0 {
-		   fmt.Printf(string(body))
+			fmt.Printf(string(body))
 		}
 
 		done <- true
 	}
 }
 
+
+// Goroutine to keep consuming <-done
+func requests_done(done, end chan bool, n, c int) {
+
+	start_time := time.Now().UnixNano()
+	last_now := start_time
+
+	for i := 0; i < n; i++ {
+		<-done
+		if i % c == 0 && i != 0 {
+			now := time.Now().UnixNano()
+			interval := (now - last_now) / 1e6
+			fmt.Printf("%d reqs done. + %d msecs\n", i, interval)
+			last_now = now
+		}
+	}
+
+	fmt.Printf("%d reqs done.\n", n)
+
+	total_time := (time.Now().UnixNano() - start_time) / 1e6
+	fmt.Printf("\ntotal time: %d msecs\n", total_time)
+
+	end <- true
+}
+
+
 func main() {
 
-	url := flag.String("url", "", "url to connect to")
+	url := flag.String("url", "", "url to connect to: 'https://www.google.com'")
 	n := flag.Int("n", 1, "total number of requests")
 	c := flag.Int("c", 1, "number of parallel requests")
 
@@ -62,38 +88,51 @@ func main() {
 	VERBOSE = *verbose
 
 	// Enable debug in gospdy code
-	if VERBOSE > 10 {
+	if VERBOSE > 2 {
 		spdy.Log = func(format string, args ...interface{}) {
-		    fmt.Printf(format, args...)
+			fmt.Printf(format, args...)
 		}
 	}
+
+	start := make(chan bool, *n)
+	done := make(chan bool, *n)
+
+
+	end := make(chan bool, *n)
+
+	// just one request
+	if *n == 1 && *c == 1 {
+
+		if VERBOSE == 0 {
+			VERBOSE = 1
+		}
+
+		go connect(start, done, *url)
+		start <- true
+		<-done
+
+		return
+	}
+
+	// Multiple Requests
 
 	// Setting Max Procs to the Number of CPU Cores
 	fmt.Printf("Max procs %d\n", runtime.GOMAXPROCS(runtime.NumCPU()))
-	fmt.Printf("Max procs %d\n", runtime.GOMAXPROCS(0))
+	fmt.Printf("Max procs %d\n\n", runtime.GOMAXPROCS(0))
 
-	loop := *n / *c
-
-	done := make(chan bool, *n)
-	start := make(chan bool, *n)
-
-	b := time.Now().UnixNano()
+	go requests_done(done, end, *n, *c)
 
 	for i := 0; i < *c; i++ {
 		go connect(start, done, *url)
-		start <- true // start some goroutines immediately
+		// start some goroutines immediately
+		start <- true
 	}
+
 	for i := *c; i < *n; i++ {
-		start <- true // fill in the chan so everybody can work
+		// fill in the chan so everybody can work
+		start <- true
 	}
 
-	for i := 0; i < *n; i++ {
-		<-done
-		if i%loop == 0 && i != 0 {
-			fmt.Printf("%d reqs end in %d msecs\n", i, (time.Now().UnixNano()-b)/1e6)
-		}
-	}
-	e := time.Now().UnixNano()
-
-	fmt.Printf("time used: %dmsecs\n", (e-b)/1e6)
+	// wait for all the requests to be terminated
+	<-end
 }
